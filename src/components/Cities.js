cd885b26a7265e3103683e066fd316ed01ad444d
baewoojin514 <dnwls514@ajou.ac.kr>
2020-07-23 14:15:28 +0900

    listing available citie completed

--- src/components/Cities.js
+++ src/components/Cities.js
@@ -1,7 +1,16 @@
 import React from 'react';
+
+const API_CITIES = 'http://localhost:8888/weather-service/available-cities';
+
 class Cities extends React.Component {
   componentDidMount() {
     console.log('City component');
+
+      const cities = fetch(API_CITIES)
+          .then(res => res.json())
+          .then(data => console.log(data))
+
+    console.log(cities);
   }
   //class 내부에는 render가 반드시 존재해야 한다.
   render() {
