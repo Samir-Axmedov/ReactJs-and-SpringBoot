e6dff71391945119df433962fa052a986f348bc2
baewoojin514 <dnwls514@ajou.ac.kr>
2020-07-23 13:21:11 +0900

    home and about path are set

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import {BrowserRouter} from 'react-router-dom';
 import './App.css';
-import Header from './components/Header'
+//import Header from './components/Header'
 import RouterApp from './RouterApp';
 
 
 
 function App() {
-  const cityName = "Seoul";
+  //const cityName = "Seoul";
   return (
     <BrowserRouter>
       <RouterApp />
--- src/RouterApp.js
+++ src/RouterApp.js
@@ -3,14 +3,13 @@ import { Route } from 'react-router-dom';
 import Header from './components/Header';
 //import Navigation from './components/Navigation';
 import Home from './components/Home';
-//import About from './components/About';
+import About from './components/About';
 const RouterApp = () => {
   const cityName = "Seoul";
   return (
     <div className="App">
       <Header cityName={cityName} />
       <p>Hello world!</p>
-      <Navigation />
       <Route exact path="/" component={Home} />
       <Route path="/about" component={About} />
     </div>
